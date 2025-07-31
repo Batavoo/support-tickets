5d9f0fea4760393b602cd82394ba494e5af18c59
Gustavo Santos Arruda <gsarruda.office@gmail.com>
2025-07-31 16:53:43 -0300

    obtendo params das rotas

---
+++ src/controllers/tickets/index.js
@@ -0,0 +1,12 @@
+/*
+ * CREATE - criar
+ * INDEX - listar
+ * UPDATE - atualizar
+ * REMOVE - remover
+ * SHOW - para exibir um único registro
+ */
+
+export function index({ request, response, database }) {
+  const tickets = database.select("tickets");
+  return response.end(JSON.stringify(tickets));
+}
--- src/middlewares/routeHandler.js
+++ src/middlewares/routeHandler.js
@@ -1,12 +1,15 @@
 import { routes } from "../routes/index.js";
+import { Database } from "../database/database.js";
+
+const database = new Database();
 
 export function routeHandler(request, response) {
   const route = routes.find((route) => {
-    return route.path === request.url && route.method === request.method;
+    return route.path.test(request.url) && route.method === request.method;
   });
 
   if (route) {
-    return route.controller({ request, response });
+    return route.controller({ request, response, database });
   }
 
   return response.writeHead(404).end();
